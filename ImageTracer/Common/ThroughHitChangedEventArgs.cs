01455792f94ca64396fc52761c9021f965b7683b
kusa-mochi <whoatemyapplepie@gmail.com>
2019-11-02 23:06:32 +0900

    add setting item to enable click through MainWindow.

---
+++ ImageTracer/Common/ThroughHitChangedEventArgs.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageTracer.Common
+{
+    public class ThroughHitChangedEventArgs : EventArgs
+    {
+        public bool NewValue { get; set; }
+    }
+}
--- ImageTracer/ViewModels/MainWindowViewModel.cs
+++ ImageTracer/ViewModels/MainWindowViewModel.cs
@@ -14,6 +14,7 @@ using Livet.Messaging.IO;
 using Livet.EventListeners;
 using Livet.Messaging.Windows;
 
+using ImageTracer.Common;
 using ImageTracer.Views;
 using ImageTracer.Models;
 
@@ -66,6 +67,12 @@ namespace ImageTracer.ViewModels
         public void Initialize()
         {
         }
+
+        #region イベント
+
+        public event EventHandler<ThroughHitChangedEventArgs> ThroughHitChanged;
+
+        #endregion
 
         #region CurrentImage変更通知プロパティ
         private BitmapImage _CurrentImage = null;
@@ -233,6 +240,31 @@ namespace ImageTracer.ViewModels
                 RaisePropertyChanged();
             }
         }
+        #endregion
+
+        #region ThroughHit変更通知プロパティ
+
+        private bool _ThroughHit = false;
+
+        public bool ThroughHit
+        {
+            get
+            {
+                return _ThroughHit;
+            }
+
+            set
+            {
+                if (_ThroughHit == value) return;
+                _ThroughHit = value;
+                if (ThroughHitChanged != null)
+                {
+                    ThroughHitChanged.Invoke(this, new ThroughHitChangedEventArgs { NewValue = value });
+                }
+                RaisePropertyChanged();
+            }
+        }
+
         #endregion
 
         #region ShowSettingDialogCommand
