acbb97a7fadf7e2bad7a045d89644a3752956a23
kusa-mochi <whoatemyapplepie@gmail.com>
2021-08-15 15:34:31 +0900

    make scratch electron app base.

---
+++ image-tracer/src/main.js
@@ -0,0 +1,12 @@
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+new Vue({
+  store,
+  render: function (h) {
+    return h(App);
+  },
+}).$mount("#app");
